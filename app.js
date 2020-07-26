590ddcd6e01d6875da7ac9d8d630d4810c82a2e8
Andres Ramirez <andresaaramirez@aol.com>
2020-07-26 19:18:18 -0400

    i logged info to the local storage

--- app.js
+++ app.js
@@ -3,16 +3,10 @@ var currentDay = moment().format("LLLL");
 document.querySelector("#currentDay").innerHTML = currentDay;
 ///////////////////////////////////////////////////////////////
 
-var saveBox = $(".fa-save");
-console.log(saveBox);
-
 //format time for input boxes
 let militaryTime = moment().format("HH");
 
-//hold militarytime and compare to another number
-let MilitaryTimeInt = parseInt(militaryTime);
-console.log(militaryTime)
-
+//cited: worked with tutor//
 //set the attributes to its time input block
 $("#text9").attr("hour", moment("9:00 AM", "h:mm a").format("HH"));
 $("#text10").attr("hour", moment("10:00 AM", "h:mm a").format("HH"));
@@ -25,68 +19,76 @@ $("#text16").attr("hour", moment("4:00 PM", "h:mm a").format("HH"));
 $("#text17").attr("hour", moment("5:00 PM", "h:mm a").format("HH"));
 
 
-$(document).ready(function() {
+$(document).ready(function(e) {
 //$(".container").find("textarea");
 console.log($(".container").find("textarea"))
 
 //input box color changes based on the hour of the day
-$('textarea').each(function (_, el){
-
+//works cited: worked on with tutor
+$('textarea').each(function (i,el){
     var elId = $(el).attr('id');
-    var textHour = +elId.replace('text', '');
+    var textHour = +elId.replace('text','');
     var currentHour = moment().hour();
-    //var currentHour = 12;
+    //var currentHour = 9;
 
     if (textHour === currentHour) {
         $(el).addClass('present');
 
-    } else if(textHour > currentHour ) {
+    } else if(textHour > currentHour) {
         $(el).addClass('future');
     }
     else {
         $(el).addClass('past');
-    }
-});
-
-
-
-
-/*for (let i = 9; i < 17; i++) {
-
-    var textInput = $("textarea").attr('hour');
-    console.log(textInput);
-    var textInputInt = parseInt(textInput);
-    console.log(textInputInt)
-
-    if (MilitaryTimeInt > textInputInt) {
-        textInput.addClass(".past")
-        console.log(textInput.addClass(".past"))
-    }
-}*/
-
-
-
-
-//button to save to local storage
-saveBox.on("mouseenter", function () {
-    $(this).addClass('hover');
-});
-
-
-//click button will save userinput to storage
-saveBox.on("click", function() {
-    const elements = document.getElementsByTagName("button");
-    
+    }  
 
 });
 
+/////local storage section
+
+//button for saving users input
+var saveBox = $(".fa-save");
+console.log(saveBox);
+
+var textBox = $('textarea');
+console.log(textBox);
+
+/*
+$('textarea').keypress(function () {
+    //var hour = $(this).attr('hour')
+    var plans = $(this).val();
+    console.log(hour)
+
+    const saveToStorage = localStorage.setItem(plans, hour);
+    localStorage.getItem(plans);
+
+});*/
 
 
+
+
+saveBox.on('click', function() {
+    //e.preventDefault();
+    $('textarea').each(function() {
+         // set a variable to select the textareas attributes
+        var hour = $(this).attr('hour');
+        //console.log(hour)
+        //set a variable to select value of users input at certain hour
+        var plans = $(this).val();
+        //i saved the hour's plan to local storage
+        localStorage.setItem(hour, plans);
+        //var hourInput = localStorage.getItem(hour)
+    });
 });
 
+function makePlans(i) {
+    //populate hours into local storage
+    $('textarea').each(function() {
+        var time = localStorage.getItem([i])
+    });
+   
+}
+makePlans();
 
-
-
-
-
+//write code to save plans to calendar
+});
 
